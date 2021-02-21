99ce0b136d1dae8d86809166aca5817983d8c666
James Edens <jedensuscg@gmail.com>
2021-02-20 20:31:12 -0500

    inital front end coding

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ const getTaf = require("./utils/getTaf");
 const { response } = require("express");
 const fs = require("fs");
 require("dotenv").config();
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 3000;
+const indexPath = path.join(__dirname, "../public")
 
-app.get("/" , (req, res) => {
-  
-})
+app.get('/', (req, res) => {
+  console.log(__dirname)
+  res.sendFile(path.resolve('./public/index.html'));
+});
 
 app.get("/taf", (req, res) => {
   if (process.env.NODE_ENV == "development") {
