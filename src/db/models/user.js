5c0061d7acaf08efd607c4bf941a4b621b4b7dd2
James Edens <jedensuscg@gmail.com>
2021-04-30 12:17:35 -0400

    changed  error logging for user model

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcryptjs")
-const jwt = require('jsonwebtoken')
-
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   first_name: {
@@ -55,56 +53,67 @@ const userSchema = new mongoose.Schema({
     },
     trim: true,
   },
-  tokens: [{
-    token: {
-      type: String,
-      required: true,
-    }
-  }],
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 //puts method on instance of User
 userSchema.methods.generateAuthToken = async function () {
-  const user = this
+  const user = this;
 
-  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+  const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
 
-  user.tokens = user.tokens.concat({ token })
-  await user.save()
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
 
-  return token
-
-}
+  return token;
+};
 
 //Puts method on Model(Model Method)
 userSchema.statics.findByCredentials = async (email, password) => {
-  try {   
+  // try {
 
-    const user = await User.findOne({email})
-    if (!user) {
-      throw new Error("Unable to login")
-    }
+  //   const user = await User.findOne({email})
+  //   if (!user) {
+  //     throw new Error("Unable to login")
+  //   }
 
-    const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) {
-      throw new Error('Unable to login')
-    }
+  //   const isMatch = await bcrypt.compare(password, user.password)
+  //   if (!isMatch) {
+  //     throw new Error('Unable to login')
+  //   }
 
-    return user
-  } catch (error) {
-    console.log(error)
+  //   return user
+  // } catch (error) {
+  //   console.log(error)
+  // }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
   }
 
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
 
-}
+  return user;
+};
 
 // Hash Plaintext password before saving
 userSchema.pre("save", async function (next) {
   const user = this;
 
-if (user.isModified('password')) {
-  user.password = await bcrypt.hash(user.password, 8)
-}  
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
   next();
 });
--- src/routers/user.js
+++ src/routers/user.js
@@ -20,6 +20,7 @@ router.post("/users/login", async (req, res) => {
     const token = await user.generateAuthToken()
     res.send({ user, token });
   } catch (error) {
+    console.log(error)
     res.status(400).send(error);
   }
 });
