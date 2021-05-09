76e9b0e5c32a7984627194bcfd25f5aea2b8fff5
James Edens <jedensuscg@gmail.com>
2021-05-09 10:58:00 -0400

    Reoved extra console logs

--- src/middleware/unitAdmin.js
+++ src/middleware/unitAdmin.js
@@ -21,21 +21,18 @@ const unitAdmin = async(req, res, next) => {
     }
 
     if (req.method === "POST") {
-      console.log(user.unitAdminPermissions[0].create)
       if (!user.unitAdminPermissions[0].create) {
         throw new Error("Invalid Permission to access this route.")
       }
     }
 
     if (req.method === "PATCH") {
-      console.log(user.unitAdminPermissions[1].patch)
       if (!user.unitAdminPermissions[1].patch) {
         throw new Error("Invalid Permission to access this route.")
       }
     }
 
     if (req.method === "DELETE") {
-      console.log(user.unitAdminPermissions[2].delete)
       if (!user.unitAdminPermissions[2].delete) {
         throw new Error("Invalid Permission to access this route.")
       }
