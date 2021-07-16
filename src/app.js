fd7079ff6edf4d3ae4a7a450fae56d580eaca799
James Edens <jedensuscg@gmail.com>
2021-07-16 15:45:16 -0400

    inital changes to add metar to fetch

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const fs = require("fs");
 require("dotenv").config();
 require("./db/mongoose");
+const getMetar = require("./utils/getMetar")
 const getTaf = require("./utils/getTaf");
 const Unit = require("./models/unit");
 const unitRouter = require('./routers/unit')
@@ -40,13 +41,21 @@ app.get("/taf/:unit", async (req, res) => {
   } catch (error) {}
 
   if (process.env.NODE_ENV == "development") {
+    let metarData;
     const fs = require("fs");
     const xmlTestData = fs.readFileSync("./devOps/tafdata.xml", "utf8");
+    const xmlTestMetarData = fs.readFileSync("./devOps/metardata.xml", "utf8");
     console.log("app.js", "Using DEV taf file");
     getTaf({ test: true, dataSource: xmlTestData })
       .then((response) => {
         response["airStation"] = unit;
-        res.send(response);
+        return response
+      }).then((response) => {
+        getMetar({ test: true, dataSource: xmlTestMetarData }).then((metar) => {
+          metarData = metar
+          response["METAR"] = metarData;
+          res.send(response)
+        })
       })
       .catch((error) => {
         console.log("ERROR", error);
@@ -65,8 +74,39 @@ app.get("/taf/:unit", async (req, res) => {
   }
 });
 
+app.get("/metar/:unit", async (req, res) => {
+  const _unit = req.params.unit;
+  let unit; 
+  if (process.env.NODE_ENV == "development") {
+    const fs = require("fs");
+    const xmlTestData = fs.readFileSync("./devOps/metardata.xml", "utf8");
+    console.log("app.js", "Using DEV metar file");
+    getMetar({ test: true, dataSource: xmlTestData })
+      .then((response) => {
+        console.log(xmlTestData)
+        res.send(response);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
+  } else {
+    getMetar({ test: false, dataSource: _unit})
+      .then((response) => {
+        response["airStation"] = unit;
+        res.send(response);
+      })
+      .catch((error) => {
+        console.log('ERROR:', error);
+      });
+  }
+});
+
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+
+
+
 
 
