213f3ecb789dfe5244f67bfd9b7dc0c043e515fa
James Edens <jedensuscg@gmail.com>
2024-08-09 00:42:46 -0500

    Added update notes

--- public/js/data.js
+++ public/js/data.js
@@ -193,8 +193,6 @@ function changeIndicatorToLongForm (changeIndicator) {
   return longFormChangeIndicator
 };
 
-
-
 function formatNumberToThreeDigits (value) {
   //if value is not integer, return value
   if (!isNaN(value)) {
@@ -203,5 +201,30 @@ function formatNumberToThreeDigits (value) {
   } else {
     return value
   }
+}
+
+let modal = document.getElementById("updateModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("update-btn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
   
